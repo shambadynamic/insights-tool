f564a4bf0e6992de7f3a5be2c38e943f42037e7a
SyedAsadKazmi <syedasadkazmi786@gmail.com>
2022-01-16 15:53:25 +0000

    New commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public-flutter')));
+
+
+app.listen(5000, () =>  console.log(`Listening on port 5000`))
